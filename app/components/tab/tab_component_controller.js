ecc64bc8f7b809daf6b907c57aab0c9819e4255f
skyler <pkphung2001@gmail.com>
2023-08-12 00:36:32 +0700

    Update Tab component

--- app/components/tab/tab_component_controller.js
+++ app/components/tab/tab_component_controller.js
@@ -1,7 +1,25 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["header", "head", "contents", "content"]
+  static values = {
+    initTabIndex: { type: Number, default: 0 },
+    currentTabIndex: Number
+  }
   connect() {
-    console.log("Hello, Stimulus!", this.element);
+    // console.log(this.initTabIndexValue)
+  }
+  initialize() {
+    this.headTarget.querySelector('details').open = true
+    this.contentTarget.classList.remove('hidden')
+  }
+  openContent(event) {
+    // hide current content to open another one
+    this.contentTargets[this.currentTabIndexValue].classList.add('hidden')
+    // dis-open current head
+    this.headTargets[this.currentTabIndexValue].querySelector('details').removeAttribute('open')
+    const currentHead = event.target.closest('[data-controller]')
+    this.currentTabIndexValue = this.headTargets.indexOf(currentHead)
+    this.contentTargets[this.currentTabIndexValue].classList.remove('hidden')
   }
 }
