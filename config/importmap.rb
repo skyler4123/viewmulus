3fa9ed56a6c0610d85ca8d404f48dfbf83664484
skyler <pkphung2001@gmail.com>
2023-08-11 18:13:56 +0700

    pin dayjs + trix

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/components", under: "controllers", to: "components"
+pin "trix", to: "https://ga.jspm.io/npm:trix@2.0.5/dist/trix.esm.min.js"
+pin "dayjs", to: "https://ga.jspm.io/npm:dayjs@1.11.9/dayjs.min.js"
