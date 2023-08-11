7fed1690539de778708bd92f8b85cb01a3cc538e
skyler <pkphung2001@gmail.com>
2023-08-11 18:46:24 +0700

    Config for autoload JS in components

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/components", under: "controllers", to: "components"
+pin_all_from "app/components", under: "controllers", to: "components", preload: true
 pin "trix", to: "https://ga.jspm.io/npm:trix@2.0.5/dist/trix.esm.min.js"
 pin "dayjs", to: "https://ga.jspm.io/npm:dayjs@1.11.9/dayjs.min.js"
