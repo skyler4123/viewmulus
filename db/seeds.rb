ed83d201d32f2d3db68351c5e25163e9be2a85eb
phong <phong.phanthanh@hdwebsoft.dev>
2023-08-21 13:41:52 +0700

    WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,4 +14,25 @@ ActiveRecord::Base.transaction do
   10.times do
     User.all.sample.posts.create!(content: Faker::Quote.famous_last_words)
   end
+  5.times do
+    User.all.sample.singer.create!(name: Fker::Name.name)
+  end
+  5.times do
+    User.all.sample.shop.create!(name: Fker::Name.name)
+  end
+  5.times do
+    Event.create!(name: Faker::Name.name)
+  end
+  5.times do
+    Song.create!(name: Faker::Name.name)
+  end
+  5.times do
+    User.all.sample.playlists.create
+  end
+  5.times do
+    User.all.sample.your_musics.create()
+  end
+  5.times do
+    Singer.all.sample.albums.create!(name: Faker::Name.name)
+  end
 end
