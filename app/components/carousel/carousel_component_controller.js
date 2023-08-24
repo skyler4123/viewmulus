54850ccb392a9625a7dcee0dc7f26b6557d47396
phong <phong.phanthanh@hdwebsoft.dev>
2023-08-24 13:40:58 +0700

    Add Carousel

--- app/components/carousel/carousel_component_controller.js
+++ app/components/carousel/carousel_component_controller.js
@@ -1,7 +1,20 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["carousel"]
+
   connect() {
     // console.log("Hello, Stimulus!", this.element);
+  }
+  initialize() {
+    // this.element.querySelectorAll("")
+  }
+
+  previous() {
+    this.carouselTarget.scrollBy(-1, 0)
+  }
+
+  next() {
+    this.carouselTarget.scrollBy(1, 0)
   }
 }
--- app/components/popover/left_component_controller.js
+++ app/components/popover/left_component_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
-    console.log("Hello, Stimulus!", this.element);
+    // console.log("Hello, Stimulus!", this.element);
   }
   initialize() {
     this.element.parentNode.closest('[data-controller]').classList.add('relative', 'group')
