c642306cb72a8728a8d02f5ff3b7e0d13d84fa1d
skyler <pkphung2001@gmail.com>
2023-08-27 17:52:27 +0700

    Prepare ViewComponent for Boxchat

--- app/components/boxchat/form_component_controller.js
+++ app/components/boxchat/form_component_controller.js
@@ -1,6 +1,11 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["messgaes"]
+  static values = {
+    boxchatId: String,
+    currentUser: Object 
+  }
   connect() {
     console.log("Hello, Stimulus!", this.element);
   }
