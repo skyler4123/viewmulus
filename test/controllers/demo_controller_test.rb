3874d40552e96cea522334b18f458fb8764d0022
skyler <pkphung2001@gmail.com>
2024-04-28 21:42:36 +0700

    update

---
+++ test/controllers/demo_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class DemoControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get demo_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get demo_new_url
+    assert_response :success
+  end
+end
