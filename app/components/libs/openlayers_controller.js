f65be98484642b06814aaf6de4919f64cc784658
skyler <pkphung2001@gmail.com>
2024-04-29 12:09:12 +0700

    update

--- app/components/libs/openlayers_controller.js
+++ app/components/libs/openlayers_controller.js
@@ -37,13 +37,7 @@ export default class Openlayers extends ApplicationController {
     }
   }
 
-  // init() {
-  //   super.init()
-  //   this.initComplete()
-  // }
-
   initParams() {
-    // super.initParams()
     this.setParams({name: 'type', defaultValue: 'default'})
   }
 
@@ -54,44 +48,28 @@ export default class Openlayers extends ApplicationController {
   clear() {
     this.source.clear()
   }
+
   connect() {
-    function clamp(value, low, high) {
-      return Math.max(low, Math.min(value, high));
-    }
+
     
-    function getColor(feature) {
-      const min = 1e8; // the smallest area
-      const max = 2e13; // the biggest area
-      const steps = 50;
-      const ramp = colormap({
-        colormap: 'blackbody',
-        nshades: steps,
-      });
-      
-      const area = getArea(feature.getGeometry());
-      const f = Math.pow(clamp((area - min) / (max - min), 0, 1), 1 / 2);
-      const index = Math.round(f * (steps - 1));
-      return ramp[index];
-    }
-    
-    this.source = new VectorSource()
-    this.sourceOSM = new OSM()
+    this.vectorSource = new VectorSource()
+    this.osmSource = new OSM()
 
     this.map = new Map({
       target: this.mapTarget,
       view: new View({
-        center: [116.6888326e5, 10.7789988e5],
-        zoom: 8,
+        center: [0, 0],
+        zoom: 2,
       }),
       layers: [
         new TiltLayer({
-          source: this.sourceOSM
+          source: this.osmSource
         })
       ]
     })
 
     const layer = new VectorLayer({
-      source: this.source,
+      source: this.vectorSource,
       style: (feature) => {
         return new Style({
           fill: new Fill({
@@ -111,56 +89,34 @@ export default class Openlayers extends ApplicationController {
     this.map.addLayer(layer)
     this.map.addInteraction(
       new DragAndDrop({
-        source: this.source,
+        source: this.vectorSource,
         formatConstructors: [GeoJSON]
       })
     )
 
     this.map.addInteraction(
       new Modify({
-        source: this.source,
+        source: this.vectorSource,
       })
     );
 
     this.map.addInteraction(
       new Draw({
         type: 'Polygon',
-        source: this.source,
+        source: this.vectorSource,
       })
     );
 
     this.map.addInteraction(
       new Snap({
-        source: this.source
+        source: this.vectorSource
       })
     )
 
-    const format = new GeoJSON({featureProjection: 'EPSG:3857'})
-    this.source.on('change', () => {
-      const features = this.source.getFeatures()
-      const json = format.writeFeatures(features)
-      this.downloadTarget.href = 'data:application/json;charset=utf-8,' + encodeURIComponent(json)
-    })
 
 
 
-    navigator.geolocation.watchPosition((pos) => {
-      const coords = [pos.coords.longitude, pos.coords.latitude];
-      // console.log(coords)
-      // this.map.setView(new View({
-      //   center: [pos.coords.longitude, pos.coords.latitude],
-      //   zoom: 10
-      // }))
-      
-      // const accuracy = circular(coords, pos.coords.accuracy);
-      // this.sourceOSM.clear(true);
-      // this.sourceOSM.addFeatures([
-      //   new Feature(
-      //     accuracy.transform('EPSG:4326', this.map.getView().getProjection())
-      //   ),
-      //   new Feature(new Point(fromLonLat(coords))),
-      // ]);
-    });
+
 
 
   } // connect
