18928894661cdb3f5616ece79bd61892bec72a79
skyler <pkphung2001@gmail.com>
2024-05-04 15:16:05 +0700

    update

---
+++ app/components/libs/openlayers/overlay_component.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Libs::Openlayers::OverlayComponent < ApplicationComponent
+
+  erb_template <<-ERB
+    <<%= @tag %> class="w-full h-[500px]" data-controller="<%= @controllers %>" data-<%= @controller_name %>-params-value="<%= @params %>">
+      <div class="w-full h-full" data-<%= @controller_name %>-target="map"></div>
+      <div id="popup">PopUpppppppppp pppppppp</div>
+    </<%= @tag %>>
+  ERB
+
+end
